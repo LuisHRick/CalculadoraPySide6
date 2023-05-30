1ea8c3179ccdb538c541b755ec6f88546ba9d84b
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-05-30 14:59:26 -0300

    more

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import sys
 
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QApplication, QLineEdit
+from PySide6.QtWidgets import QApplication
 
 from display import Display
+from info import Info
 from main_window import MainWindow
 from variables import WINDOW_ICON_PATH
 
@@ -16,8 +17,13 @@ if __name__ == '__main__':
     window.setWindowIcon(icon)
     app.setWindowIcon(icon)
 
+    # Info
+    info = Info('2.0 ^ 10.0 = 1024')
+    window.addToVLayout(info)
+
     # Display
     display = Display()
+    display.setPlaceholderText('Digite aqui')
     window.addToVLayout(display)
 
     # Executa tudo
