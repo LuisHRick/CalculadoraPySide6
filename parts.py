12eed378f4f0059b0781867cab2151bff3cf0823
Rickworkspace0305 <122179976+LuisHRick@users.noreply.github.com>
2023-06-05 15:57:05 -0300

    fix

--- parts.py
+++ parts.py
@@ -53,19 +53,18 @@ class Button(QPushButton):
         
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
 
         self._gridMask = [
-            ['C', '<', '^', '/'],
+            ['C', '◀', '^', '/'],
             ['7', '8', '9', '*'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
             ['',  '0', '.', '='],
         ]
-        
         self._makeGrid()
-    
+
     def _makeGrid(self):
         for rowNumber, rowData in enumerate(self._gridMask):
             for colNumber, buttonText in enumerate(rowData):
